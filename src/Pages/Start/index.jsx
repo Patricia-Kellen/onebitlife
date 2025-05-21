df613dff9bfc4cec04a9b70b62382b245cf626f9
Patricia Kellen <142345689+Patricia-Kellen@users.noreply.github.com>
2025-05-21 19:05:24 -0300

    Inserindo botão padrão com estilos

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,37 +1,53 @@
-import React from "react";
-import {View, ScrollView, Text, Image, StyleSheet} from "react-native"
-import LifeStatus from "../../components/Common/LifeStatus";
+import React, { useEffect } from "react";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import {useNavigation} from "@react-navigation/native"
 
-export default function Start(){
-    return(
-        <View style={styles.container}>
-            <ScrollView showVerticalScrollIndicator={false}>
-            <View style={{alignItems:"center"}}>
-                <Image source={require("../../assets/icons/logo3.png")}
-                style={styles.logo}/>
-                <LifeStatus/>
-                <Text style={styles.description}>Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o melhor nível.</Text>
-            </View>
-            </ScrollView>
+import LifeStatus from "../../components/Common/LifeStatus";
+import DefaultButton from "../../components/Common/DefaultButton";
+
+export default function Start() {
+const handleNavAppExplanation = () =>{
+  console.log("testanndo o click")
+}
+
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={require("../../assets/icons/logo3.png")}
+            style={styles.logo}
+          />
+          <LifeStatus />
+          <Text style={styles.description}>
+            Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"} o melhor nível.
+          </Text>
+          <DefaultButton
+          buttonText={"Continuar"}
+          handlePress={handleNavAppExplanation}
+          width={250}
+          height={50}/>
         </View>
-    )
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:"rgba(21, 21, 21, 0.98)",
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
   },
-  logo:{
+  logo: {
     width: 300,
-    height:60,
-    marginTop:60,
+    height: 60,
+    marginTop: 60,
     marginBottom: 20,
   },
-  description:{
-    color:"#FFFFFF",
+  description: {
+    color: "#FFFFFF",
     fontSize: 20,
     textAlign: "center",
     marginVertical: 60,
   },
-})
+});
